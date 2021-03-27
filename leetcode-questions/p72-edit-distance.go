d03b5e22881ca31078e139d0d75b44fb900fad8b
mchozhang <vivamcho@gmail>
2021-03-27 14:39:24 +1100

    p72

--- leetcode-questions/p72-edit-distance.go
+++ leetcode-questions/p72-edit-distance.go
@@ -7,5 +7,35 @@
 package main
 
 func minDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i <= len(word1); i++ {
+		dp[i] = make([]int, len(word2)+1)
+	}
 
+	// first row
+	for i := 0; i <= len(word2); i++ {
+		dp[0][i] = i
+	}
+
+	// first column
+	for i := 1; i <= len(word1); i++ {
+		dp[i][0] = i
+	}
+	for i := 1; i <= len(word1); i++ {
+		for j := 1; j <= len(word2); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				min := dp[i-1][j-1]
+				if dp[i-1][j] < min {
+					min = dp[i-1][j]
+				}
+				if dp[i][j-1] < min {
+					min = dp[i][j-1]
+				}
+				dp[i][j] = min + 1
+			}
+		}
+	}
+	return dp[len(word1)][len(word2)]
 }
