f3662cfb151f61fb985c2ab6a6d967dc753b95ac
mchozhang <vivamcho@gmail>
2021-03-31 19:21:48 +1100

    p234

--- leetcode-questions/p234-palindrome-linked-list.go
+++ leetcode-questions/p234-palindrome-linked-list.go
@@ -2,6 +2,30 @@
  * LeetCode : Palindromic Substrings
  * https://leetcode.com/problems/palindromic-substrings/
  *
- * submission : faster than 100%
+ * submission : faster than 53%
  */
 package main
+
+func isPalindrome(head *ListNode) bool {
+	slow := head
+	fast := head
+	var pre *ListNode
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		next := slow.Next
+		slow.Next = pre
+		pre = slow
+		slow = next
+	}
+	if fast != nil {
+		slow = slow.Next
+	}
+	for pre != nil {
+		if pre.Val != slow.Val {
+			return false
+		}
+		pre = pre.Next
+		slow = slow.Next
+	}
+	return true
+}
