db87da959aaf9747ed1ae179595c4d92d9745bc2
mchozhang <vivamcho@gmail>
2021-04-01 01:03:22 +1100

    p61

--- leetcode-questions/p61-rotate-list.go
+++ leetcode-questions/p61-rotate-list.go
@@ -1,11 +1,31 @@
 /**
- * LeetCode : Product of Array Except Self
- * https://leetcode.com/problems/product-of-array-except-self/
+ * LeetCode : Rotate List
+ * https://leetcode.com/problems/rotate-list/
  *
- * submission : faster than 24%
+ * submission : faster than 100%
  */
 package main
 
 func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	count := 1
+	tail := head
+	for ;tail.Next != nil; tail = tail.Next {
+		count++
+	}
+	k %= count
+	if k == 0 {
+		return head
+	}
 
+	node := head
+	for i := 0; i < count - k - 1; i++ {
+		node = node.Next
+	}
+	res := node.Next
+	node.Next = nil
+	tail.Next = head
+	return res
 }
