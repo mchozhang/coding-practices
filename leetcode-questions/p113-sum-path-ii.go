f7f101a9df9a026023620697cde9437a930573bc
mchozhang <vivamcho@gmail>
2021-04-02 22:53:20 +1100

    p113

---
+++ leetcode-questions/p113-sum-path-ii.go
@@ -0,0 +1,31 @@
+/**
+ * LeetCode : Path Sum
+ * https://leetcode.com/problems/path-sum-ii/
+ *
+ * submission : faster than 100%
+ */
+package main
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	res := make([][]int, 0)
+	if root != nil {
+		traverse(root, targetSum, 0, []int{}, &res)
+	}
+	return res
+}
+
+func traverse(root *TreeNode, targetSum int, currentSum int, currentPath []int, result *[][]int) {
+	arr := append([]int{}, currentPath...)
+	arr = append(arr, root.Val)
+	currentSum += root.Val
+	if root.Left == nil && root.Right == nil && currentSum == targetSum {
+		*result = append(*result, arr)
+		return
+	}
+	if root.Left != nil {
+		traverse(root.Left, targetSum, currentSum, arr, result)
+	}
+	if root.Right != nil {
+		traverse(root.Right, targetSum, currentSum, arr, result)
+	}
+}
