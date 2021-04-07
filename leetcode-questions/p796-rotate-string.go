69d6fac80af3516d2d58f95b0952c3f087d9570b
mchozhang <vivamcho@gmail>
2021-04-07 16:57:49 +1000

    p796

--- leetcode-questions/p796-rotate-string.go
+++ leetcode-questions/p796-rotate-string.go
@@ -1,1 +1,32 @@
+/**
+ * 796. Rotate String
+ * https://leetcode.com/problems/rotate-string/
+ *
+ * submission: faster than 100%
+ */
 package main
+
+import (
+	"strings"
+)
+
+func rotateString(A string, B string) bool {
+	return len(A) == len(B) && strings.Contains(A+A, B)
+}
+
+func rotateString2(A string, B string) bool {
+	if len(A) != len(B) {
+		return false
+	}
+	if len(A) == 0 && len(B) == 0 {
+		return true
+	}
+
+	for i := 0; i < len(A); i++ {
+		if A == B {
+			return true
+		}
+		A = string(A[len(A)-1]) + A[:len(A)-1]
+	}
+	return false
+}
