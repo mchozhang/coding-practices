23d055f25838eaecde66ae7aca8aa925a745300e
mchozhang <vivamcho@gmail>
2021-04-10 15:36:53 +1000

    p114

---
+++ leetcode-questions/p114-flatten-binary-tree-to-linked-list.go
@@ -0,0 +1,27 @@
+/**
+ * LeetCode: Flatten Binary Tree to Linked List
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ *
+ * submission: faster than 75%
+ */
+package main
+
+
+func flatten(root *TreeNode)  {
+	if root != nil {
+		root = traverse(root, nil)
+	}
+}
+
+func traverse(node *TreeNode, next *TreeNode) *TreeNode {
+	if node.Left != nil {
+		node.Right = traverse(node.Left, node.Right)
+		node.Left = nil
+		traverse(node.Right, next)
+	} else if node.Right != nil {
+		node.Right = traverse(node.Right, next)
+	} else {
+		node.Right = next
+	}
+	return node
+}
