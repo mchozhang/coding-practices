d112350c986600afe24132733bb7d4c5b32a2247
mchozhang <vivamcho@gmail>
2021-04-14 13:43:06 +1000

    p287

--- leetcode-questions/p287-find-the-duplicate-number.go
+++ leetcode-questions/p287-find-the-duplicate-number.go
@@ -1,1 +1,21 @@
+/**
+ * LeetCode: Find the Duplicate Number
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ *
+ * submission: faster than 99%
+ */
 package main
+
+func findDuplicate(nums []int) int {
+	for i := 0; i < len(nums); {
+		if nums[i] != i + 1{
+			if nums[nums[i]] == nums[i] {
+				return nums[i]
+			}
+			nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+		} else {
+			i++
+		}
+	}
+	return nums[len(nums) - 1]
+}
