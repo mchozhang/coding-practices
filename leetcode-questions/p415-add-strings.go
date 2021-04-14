712e2ffa8e41b38a416d78b7888b5c9b1ec96cc1
mchozhang <vivamcho@gmail>
2021-04-15 00:19:45 +1000

    p415

--- leetcode-questions/p415-add-strings.go
+++ leetcode-questions/p415-add-strings.go
@@ -2,10 +2,28 @@
  * LeetCode : Balanced Binary Tree
  * https://leetcode.com/problems/balanced-binary-tree/
  *
- * submission : faster than 80%
+ * submission : faster than 100%
  */
 package main
 
 func addStrings(num1 string, num2 string) string {
+	long, short := []byte(num1), []byte(num2)
+	if len(num1) < len(num2) {
+		long, short = short, long
+	}
 
+	sum := byte(0)
+	for i, j := len(long) - 1, len(short) - 1; i > - 1; i-- {
+		sum += long[i] - '0'
+		if j > -1 {
+			sum += short[j] - '0'
+			j--
+		}
+		long[i] = sum%10 + '0'
+		sum /= 10
+	}
+	if sum != byte(0) {
+		long = append([]byte{'1'}, long...)
+	}
+	return string(long)
 }
