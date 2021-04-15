71850875d4be4338028f5609b6018caa2f9e89ce
mchozhang <vivamcho@gmail>
2021-04-15 17:21:38 +1000

    p67

--- leetcode-questions/p67-add-binary.go
+++ leetcode-questions/p67-add-binary.go
@@ -7,5 +7,22 @@
 package main
 
 func addBinary(a string, b string) string {
-
+	long, short := []byte(a), []byte(b)
+	if len(b) > len(a) {
+		long, short = short, long
+	}
+	sum := byte(0)
+	for i, j := len(long)-1, len(short)-1; i > -1; i-- {
+		sum += long[i] - '0'
+		if j > -1 {
+			sum += short[j] - '0'
+			j--
+		}
+		long[i] = sum%2 + '0'
+		sum /= 2
+	}
+	if sum != byte(0) {
+		long = append([]byte{'1'}, long...)
+	}
+	return string(long)
 }
