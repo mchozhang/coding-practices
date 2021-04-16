dda8942ef23b947fdbb2c3735619937500563fa0
mchozhang <vivamcho@gmail>
2021-04-16 13:35:01 +1000

    p441

---
+++ leetcode-questions/p441-arranging-coins.go
@@ -0,0 +1,15 @@
+/**
+ * LeetCode : Arranging Coins
+ * https://leetcode.com/problems/arranging-coins/
+ *
+ * submission : faster than 75%
+ */
+package main
+
+func arrangeCoins(n int) int {
+	i := 0
+	for ; i*(i+1)/2 < n; i++ {
+
+	}
+	return i
+}
