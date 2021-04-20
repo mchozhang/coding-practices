c7cfad5e36a17ca4fc5b47c9bb717d9d2d6a914c
mchozhang <vivamcho@gmail>
2021-04-20 17:45:20 +1000

    p84

---
+++ leetcode-questions/p84-largest-rectangle-in-histogram.go
@@ -0,0 +1,31 @@
+/**
+ * LeetCode : Largest Rectangle in Histogram
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/
+ *
+ * submission : faster than 88%
+ */
+package main
+
+func largestRectangleArea(heights []int) int {
+	heights = append(heights, 0)
+	var stack = []int{}
+	max := 0
+	for i, value := range heights {
+		for len(stack) != 0 && value < heights[stack[len(stack)-1]] {
+			height := heights[stack[len(stack)-1]]
+			stack = stack[:len(stack)-1]
+			width := i
+			if len(stack) != 0 {
+				width = i - stack[len(stack)-1] - 1
+			}
+
+			if max < height*width {
+				max = height * width
+			}
+		}
+		stack = append(stack, i)
+	}
+	return max
+}
+
+4 5 1
