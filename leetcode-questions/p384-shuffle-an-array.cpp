7a9855c6d566dcd801c59533159d87c56ed1c8fb
mchozhang <vivamcho@gmail>
2021-05-11 16:11:15 +1000

    p384

---
+++ leetcode-questions/p384-shuffle-an-array.cpp
@@ -0,0 +1,46 @@
+/**
+ * 384. Shuffle an Array
+ * https://leetcode.com/problems/shuffle-an-array/
+ * 
+ * submission: faster than 90%
+ */
+
+#include <vector>
+using namespace std;
+
+class Solution
+{
+private:
+    vector<int> nums;
+
+    void swap(int *i, int *j)
+    {
+        int t = *i;
+        *i = *j;
+        *j = t;
+    }
+
+public:
+    Solution(vector<int> &nums)
+    {
+        this->nums = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset()
+    {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle()
+    {
+        vector<int> arr(nums);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int j = rand() % (nums.size() - i);
+            swap(&arr[i], &arr[i + j]);
+        }
+        return arr;
+    }
+};
